42ee583eab2ddc247082f1105b75ca162e0b89e1
2016122458 <44885466+2016122458@users.noreply.github.com>
2018-12-16 13:38:20 +0800

    test

--- src/main/java/com/autosendemail/email/EmailApplication.java
+++ src/main/java/com/autosendemail/email/EmailApplication.java
@@ -4,8 +4,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
-import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
 @SpringBootApplication
 public class EmailApplication implements CommandLineRunner {
